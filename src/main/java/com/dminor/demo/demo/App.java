0c18d485af7e55521752268adafd7b0c4b9183dd
unknown <wenlinli@WENLINLI-PC.sohu-inc.com>
2015-08-03 10:22:26 +0800

    create file

---
+++ src/main/java/com/dminor/demo/demo/App.java
@@ -0,0 +1,13 @@
+package com.dminor.demo.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
